12fe85dfc79b1f2e2f3e65622ff18718cd12c4eb
Silver7Surfer <quick47silver@gmail.com>
2025-03-10 18:53:53 +0545

    -fix side bar not closing automatically when clicked

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -156,6 +156,7 @@ const AdminLayout = () => {
                       ? 'bg-gray-900 text-white'
                       : 'text-gray-300 hover:bg-gray-700 hover:text-white'
                   } group flex items-center px-2 py-2 text-sm font-medium rounded-md`}
+                  onClick={() => setSidebarOpen(false)}
                 >
                   <div className={`${
                     isActive ? 'text-white' : 'text-gray-400 group-hover:text-white'
